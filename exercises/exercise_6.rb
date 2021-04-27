40657bfd5a6299237c8d6c5d2e2a3e4d23f43ddf
Penny Clark <respect.cactus@gmail.com>
2021-04-27 03:25:11 +0000

    Add logic for exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Egg", last_name: "Eggerson", hourly_rate: 60)
+@store1.employees.create(first_name: "Hello", last_name: "There", hourly_rate: 60)
+@store1.employees.create(first_name: "Debby", last_name: "Debson", hourly_rate: 60)
+@store1.employees.create(first_name: "Brett", last_name: "Bretterson", hourly_rate: 60)
+
+@store1.employees.create(first_name: "Archie", last_name: "Andrews", hourly_rate: 60)
+@store1.employees.create(first_name: "Veronica", last_name: "Lodge", hourly_rate: 60)
+@store1.employees.create(first_name: "Betty", last_name: "Cooper", hourly_rate: 60)
