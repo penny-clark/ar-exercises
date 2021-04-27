c9f74800023acf058366a83f5dac7cb17f5939bd
Penny Clark <respect.cactus@gmail.com>
2021-04-27 04:49:50 +0000

    add logic for exercise 7 including bonus

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,36 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Employee < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { greater_than: 40 }
+  validates :hourly_rate, numericality: { less_than: 200 }
+  validates :store_id, presence: true
+end
+
+class Store < ActiveRecord::Base
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { greater_than: 0 }
+  validate :store_must_have_one_type_of_apparel
+  
+  def store_must_have_one_type_of_apparel
+    if ((mens_apparel.present? == false) && (womens_apparel.present? == false))
+      errors.add(:mens_apparel, "Must sell at least one type of clothing")
+      errors.add(:womens_apparel, "Must sell at least one type of clothing")
+    end
+  end
+
+end
+
+def make_new_store
+  puts "What is the name of the store you want to add?"
+  answer = $stdin.gets.chomp
+  newstore = Store.create(name: answer)
+  newstore.valid?
+  newstore.errors.messages.each do | key, error |
+    puts "#{key}: #{error}"
+  end
+end
+
+make_new_store
