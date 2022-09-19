2736b92f6cde45baf73af0a5298d6c65a434ef07
paige-clark <paigeclarkillustration@gmail.com>
2022-09-19 19:57:06 -0400

    First two exercises

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,9 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+@store1 = Store.find_by(id: 1)
+p @store1
+@store2 = Store.find_by(id: 2)
+p @store2
+@store1.name = "Bunny's Big Ol' Store"
+p @store1
